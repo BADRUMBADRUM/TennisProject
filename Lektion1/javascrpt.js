5000e1e13c94bfb2294f19e7f1f68ad78fd8bb7e
isse911 <49311368+BADRUMBADRUM@users.noreply.github.com>
2022-11-09 10:44:07 +0100

    Pushed Project

---
+++ Lektion1/javascrpt.js
@@ -0,0 +1,6 @@
+let SaveFile = () =>
+{
+    const firstname = document.getElementById('fname');
+    const lastname = document.getElementById('lname');
+    TimeToBook = document.getElementById('bokningstid');
+}
